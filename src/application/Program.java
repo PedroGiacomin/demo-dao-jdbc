b6b49361eefd277fa48b61c22868e68103885954
Pedro Giacomin <pedrogiacomin2706@gmail.com>
2020-07-30 11:06:03 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,12 +13,12 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
-	
-		Seller seller = new Seller(21, "Bob", "Bob@gamil.com", new Date(), 3000.0, obj);
-	
-		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+	
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+	
 	}
 	
 	
